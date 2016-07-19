68263299bf88f2e93fa64ccb246a1e198f619669
pedro cuasqui <pedro160710@yahoo.com>
2016-07-18 19:01:38 -0500

    trae libros del servidor, al emulador de android

--- AdminLibrosMov/app/src/main/java/example/pedrow10/adminlibrosmov/MainActivity.java
+++ AdminLibrosMov/app/src/main/java/example/pedrow10/adminlibrosmov/MainActivity.java
@@ -3,12 +3,17 @@ package example.pedrow10.adminlibrosmov;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import example.pedrow10.adminlibrosmov.vo.Libro;
@@ -26,8 +31,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buscarLibro(View v) {
-        new BuscarLibroAsinc().execute();
-
+        BuscarLibroAsinc buscarLibroAsinc =new BuscarLibroAsinc();
+        Log.v("buscar: ","1");
+        buscarLibroAsinc.execute("FreeSoftware");
     }
 
     public class BuscarLibroAsinc extends AsyncTask<String, Void, List<Libro>> {
@@ -35,24 +41,35 @@ public class MainActivity extends AppCompatActivity {
 //    param nombre del libro para buscar
         @Override
         protected List<Libro> doInBackground(String... strings) {
-String nombreBusqueda = strings[0];
-
+            Log.v("buscar: ","2");
+//            String nombreBusqueda = strings[0];
+            String nombreBusqueda = "FreeSoftware";
+            Log.v("buscar: ","2.1");
             List listLibros = new ArrayList<Libro>();
-            listLibros.add(new Libro("Libro1","Autor1",45,1));
-            listLibros.add(new Libro("Libro2","Autor2",10,2));
-            listLibros.add(new Libro("Libro3","Autor3",18,3));
+
+/*            listLibros.add(new Libro("Libro1", "Autor1", 45, 1));
+            listLibros.add(new Libro("Libro2", "Autor2", 10, 2));
+            listLibros.add(new Libro("Libro3", "Autor3", 18, 3));*/
 //            la consulta se realiza por id
-//            final String url = "http://localhost:8080/CrudLibroRestMoviles/rest/crudLibroRest/consultarLibro?varNombre="+nombreBusqueda;
-//            final String url = "http://localhost:8080/CrudLibroRestMoviles/rest/crudLibroRest/consultarLibro?varId=1";
-//            RestTemplate restTemplate = new RestTemplate();
-//            restTemplate.getMessageConverters().add(
-//                    new MappingJackson2HttpMessageConverter());
-//            Libro libro = restTemplate.getForObject(url, Libro.class);
+//            final String url = "http://172.30.238.213:8080/CrudLibroRestMoviles/rest/crudLibroRest/consultarLibro?varTitulo="+nombreBusqueda;
+
+//            final String url = "http://172.30.238.213:8080/CrudLibroRestMoviles/rest/crudLibroRest/consultarLibro?varId=1";
+            final String url = "http://172.30.238.213:8080/CrudLibroRestMoviles/rest/crudLibroRest/consultarLibros";
+            Log.v("buscar: ","3"+ url);
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.getMessageConverters().add(
+                    new MappingJackson2HttpMessageConverter());
+            Log.v("buscar: ","3 ");
+            Libro[] librosArray = restTemplate.getForObject(url, Libro[].class);
+            //se obtiene un objeto java
+            listLibros = Arrays.asList(librosArray);
+            Log.v("buscar: ","4 son"+ listLibros.size());
             return listLibros;
         }
 
         @Override
         protected void onPostExecute(List<Libro> libros) {
+
 //            String[] datos = new String[]{"Libro1", "Libro2", "Libro3"};
 
 //            String[] datos = (String[]) libros.toArray();
